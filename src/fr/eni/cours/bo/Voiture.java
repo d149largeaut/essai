9c3661677cf22efaa33c733b99de144e6c4b8582
Thierry Largeau <thierry.largeau@gmail.com>
2019-07-12 11:08:06 +0200

    ajout de l'attribut "modele" et ses methodes accesseur/mutateur

--- src/fr/eni/cours/bo/Voiture.java
+++ src/fr/eni/cours/bo/Voiture.java
@@ -1,11 +1,32 @@
 package fr.eni.cours.bo;
 
-public class Voiture {
-	private String marque;
+import java.io.Serializable;
 
+/**
+ * JavaBean Voiture
+ * @author tl
+ * @modification ajout d'un modele (membres)
+ *
+ */
+public class Voiture implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	//attributs
+	private String marque;
+	private String modele; 
+
+	//constructeurs
 	public Voiture() {
 	}
 
+	//autres methodes
+	@Override
+	public String toString() {
+		return "Voiture [marque=" + marque + "]";
+	}
+	
+	//accesseurs et mutateurs
 	public String getMarque() {
 		return marque;
 	}
@@ -14,10 +35,13 @@ public class Voiture {
 		this.marque = marque;
 	}
 
-	@Override
-	public String toString() {
-		return "Voiture [marque=" + marque + "]";
+	public String getModele() {
+		return modele;
 	}
-	
+
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+
 	
 }
